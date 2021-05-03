723a2bbedd61bd9d92ecdd461c8e639024daca9c
dasoopark <ektn0514@gmail.com>
2021-05-03 22:17:15 +0900

    파일삭제
    파일삭제

--- Data_BindingEx/app/src/main/java/kr/co/korearental/myapplication/UserProfile.kt
+++
@@ -1,8 +0,0 @@
-package kr.co.korearental.myapplication
-
-//XML에 연결할 클래스
-public class UserProfile(firstName:String, lastName:String){
-   var firstName:String = firstName
-   var lastName:String= lastName
-
-}
