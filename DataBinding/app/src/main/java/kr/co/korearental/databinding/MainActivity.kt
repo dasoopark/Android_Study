c93e216f791c3cf3ee043714654ddef7a7ab9168
dasoopark <ektn0514@gmail.com>
2021-05-03 22:15:48 +0900

    dataBinding 기본 예제
    클래스의 변수, Xml과 연동하여 매핑 한 데이터 출력 예제

--- DataBinding/app/src/main/java/kr/co/korearental/databinding/MainActivity.kt
+++ DataBinding/app/src/main/java/kr/co/korearental/databinding/MainActivity.kt
@@ -3,9 +3,10 @@ package kr.co.korearental.databinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import kr.co.korearental.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:  ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
