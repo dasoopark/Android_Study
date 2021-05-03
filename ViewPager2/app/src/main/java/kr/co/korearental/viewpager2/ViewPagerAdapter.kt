c05e9f696780fe727f9d7ed6eede0d0303384065
dasoopark <ektn0514@gmail.com>
2021-05-04 06:07:21 +0900

    화면에 이미지를 담는 ViewPager
    화면에 이미지를 담는 ViewPager
    예시 작성 완료.

--- ViewPager2/app/src/main/java/kr/co/korearental/viewpager2/ViewPagerAdapter.kt
+++ ViewPager2/app/src/main/java/kr/co/korearental/viewpager2/ViewPagerAdapter.kt
@@ -3,7 +3,9 @@ package kr.co.korearental.viewpager2
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-
+import kotlinx.android.synthetic.main.idol_list_item.view.*
+                    //ArrayList<Int> 부분 뭐넣을지에 따라 다름 이떄 img= int로 저장되기 때문에 이걸로한거고
+                    //텍스트를 화면에 표시하고 싶을땐 String으로 담아야함.
 class ViewPagerAdapter(idolList: ArrayList<Int>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
     var item = idolList
 
@@ -17,7 +19,6 @@ class ViewPagerAdapter(idolList: ArrayList<Int>) : RecyclerView.Adapter<ViewPage
 
     inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
         (LayoutInflater.from(parent.context).inflate(R.layout.idol_list_item, parent, false)){
-
         val idol = itemView.imageView_idol!!
     }
 }
